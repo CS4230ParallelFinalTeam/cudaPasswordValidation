9147bc819421e67094fd542705f70cc8d466cf3e
Todd A Rogers <trogers@lab1-30.eng.utah.edu>
2012-11-30 18:38:29 -0700

    brute is working correctly, doesn't look pretty yet.. ill do that tmrw

--- bruteForce.cu
+++ bruteForce.cu
@@ -19,10 +19,15 @@ uint64_t x6;
 uint64_t x7;
 uint64_t x8;
 uint64_t x9;
+uint64_t x10;
+uint64_t x11;
+uint64_t x12;
+uint64_t x13;
+uint64_t x14;
 };
 
 extern void findPass_wrapper(passNum *passPtr, passNum *bruteNumOut, uint64_t *maxNum);
-extern __global__ void findPass(passNum *passPtr, passNum *bruteNumOut, uint64_t *max);
+extern __global__ void findPass(uint64_t *passPtr, uint64_t *bruteNumOut, uint64_t *max);
 extern void stringToNumaz(char *str, passNum *passPtr);
 extern void stringToNumazAZ(char *str, passNum *passPtr);
 extern void stringToNumazAZ09(char *str, passNum *passPtr);
@@ -33,125 +38,227 @@ extern int cudaFree();
 extern void __syncthreads();
 extern int cudaMemcpyToSymbol();
 __device__ bool isCracked = false;
+__device__ uint64_t bruteOut = 0;
 
 //wrapper for findPass, allocates memory then passes it to device and calls findPass
 extern void findPass_wrapper(passNum *passPtr, passNum *bruteNumOut, uint64_t *maxNum)
 {
-	passNum *devI1Ptr;
-	passNum *dev01Ptr;
+	uint64_t *devI1Ptr;
+	uint64_t *dev01Ptr;
 	uint64_t *devI2Ptr;
-	cudaMalloc((void **)&dev01Ptr, sizeof(passNum));
-    cudaMemcpy(dev01Ptr, bruteNumOut, sizeof(passNum), cudaMemcpyHostToDevice);
-    cudaMalloc((void **)&devI1Ptr, sizeof(passNum));
-    cudaMemcpy(devI1Ptr, passPtr, sizeof(passNum), cudaMemcpyHostToDevice);
-    cudaMalloc((void **)&devI2Ptr, sizeof(int));
+	
+	cudaMalloc((void **)&dev01Ptr, sizeof(uint64_t));
+    cudaMemcpy(dev01Ptr, &bruteNumOut->x1, sizeof(uint64_t), cudaMemcpyHostToDevice);
+    cudaMalloc((void **)&devI1Ptr, sizeof(uint64_t));
+    cudaMemcpy(devI1Ptr, &passPtr->x1, sizeof(uint64_t), cudaMemcpyHostToDevice);
+    cudaMalloc((void **)&devI2Ptr, sizeof(uint64_t));
     cudaMemcpy(devI2Ptr, maxNum, sizeof(uint64_t), cudaMemcpyHostToDevice);
     
-    dim3 dimGrid(43, 1);
-    dim3 dimBlock(96, 1);
-    findPass<<<dimGrid,dimBlock>>>(devI1Ptr, dev01Ptr, devI2Ptr);
-    cudaMemcpy(bruteNumOut, dev01Ptr, sizeof(passNum), cudaMemcpyDeviceToHost);
+    dim3 dimGrid(40960, 1);
+    dim3 dimBlock(512, 1);
+    
+    if(passPtr->x1 != 0)
+    {
+		findPass<<<dimGrid,dimBlock>>>(devI1Ptr, dev01Ptr, devI2Ptr);
+		cudaThreadSynchronize();
+		//cudaMemcpy(&bruteNumOut->x1, dev01Ptr, sizeof(uint64_t), cudaMemcpyDeviceToHost);
+		cudaMemcpyFromSymbol(&bruteNumOut->x1, "bruteOut", sizeof(uint64_t),0,cudaMemcpyDeviceToHost);
+		bruteOut = 0;
+    }
+    
+    if(passPtr->x2 != 0)
+    {
+		cudaMemcpy(devI1Ptr, &passPtr->x2, sizeof(uint64_t), cudaMemcpyHostToDevice);
+		cudaMemcpy(dev01Ptr, &bruteNumOut->x2, sizeof(uint64_t), cudaMemcpyHostToDevice);
+		findPass<<<dimGrid,dimBlock>>>(devI1Ptr, dev01Ptr, devI2Ptr);
+		cudaThreadSynchronize();
+		cudaMemcpyFromSymbol(&bruteNumOut->x2, "bruteOut", sizeof(uint64_t),0,cudaMemcpyDeviceToHost);
+		bruteOut = 0;
+    }
+    
+        if(passPtr->x3 != 0)
+    {
+		cudaMemcpy(devI1Ptr, &passPtr->x3, sizeof(uint64_t), cudaMemcpyHostToDevice);
+		cudaMemcpy(dev01Ptr, &bruteNumOut->x3, sizeof(uint64_t), cudaMemcpyHostToDevice);
+		findPass<<<dimGrid,dimBlock>>>(devI1Ptr, dev01Ptr, devI2Ptr);
+		cudaThreadSynchronize();
+		cudaMemcpyFromSymbol(&bruteNumOut->x3, "bruteOut", sizeof(uint64_t),0,cudaMemcpyDeviceToHost);
+		bruteOut = 0;
+    }
+    
+        if(passPtr->x4 != 0)
+    {
+		cudaMemcpy(devI1Ptr, &passPtr->x4, sizeof(uint64_t), cudaMemcpyHostToDevice);
+		cudaMemcpy(dev01Ptr, &bruteNumOut->x4, sizeof(uint64_t), cudaMemcpyHostToDevice);
+		findPass<<<dimGrid,dimBlock>>>(devI1Ptr, dev01Ptr, devI2Ptr);
+		cudaThreadSynchronize();
+		cudaMemcpyFromSymbol(&bruteNumOut->x4, "bruteOut", sizeof(uint64_t),0,cudaMemcpyDeviceToHost);
+		bruteOut = 0;
+    }
+    
+        if(passPtr->x5 != 0)
+    {
+		cudaMemcpy(devI1Ptr, &passPtr->x5, sizeof(uint64_t), cudaMemcpyHostToDevice);
+		cudaMemcpy(dev01Ptr, &bruteNumOut->x5, sizeof(uint64_t), cudaMemcpyHostToDevice);
+		findPass<<<dimGrid,dimBlock>>>(devI1Ptr, dev01Ptr, devI2Ptr);
+		cudaThreadSynchronize();
+		cudaMemcpyFromSymbol(&bruteNumOut->x5, "bruteOut", sizeof(uint64_t),0,cudaMemcpyDeviceToHost);
+		bruteOut = 0;
+    }
+    
+        if(passPtr->x6 != 0)
+    {
+		cudaMemcpy(devI1Ptr, &passPtr->x6, sizeof(uint64_t), cudaMemcpyHostToDevice);
+		cudaMemcpy(dev01Ptr, &bruteNumOut->x6, sizeof(uint64_t), cudaMemcpyHostToDevice);
+		findPass<<<dimGrid,dimBlock>>>(devI1Ptr, dev01Ptr, devI2Ptr);
+		cudaThreadSynchronize();
+		cudaMemcpyFromSymbol(&bruteNumOut->x6, "bruteOut", sizeof(uint64_t),0,cudaMemcpyDeviceToHost);
+		bruteOut = 0;
+    }
+    
+        if(passPtr->x7 != 0)
+    {
+		cudaMemcpy(devI1Ptr, &passPtr->x7, sizeof(uint64_t), cudaMemcpyHostToDevice);
+		cudaMemcpy(dev01Ptr, &bruteNumOut->x7, sizeof(uint64_t), cudaMemcpyHostToDevice);
+		findPass<<<dimGrid,dimBlock>>>(devI1Ptr, dev01Ptr, devI2Ptr);
+		cudaThreadSynchronize();
+		cudaMemcpyFromSymbol(&bruteNumOut->x7, "bruteOut", sizeof(uint64_t),0,cudaMemcpyDeviceToHost);
+		bruteOut = 0;
+    }
+    
+        if(passPtr->x8 != 0)
+    {
+		cudaMemcpy(devI1Ptr, &passPtr->x8, sizeof(uint64_t), cudaMemcpyHostToDevice);
+		cudaMemcpy(dev01Ptr, &bruteNumOut->x8, sizeof(uint64_t), cudaMemcpyHostToDevice);
+		findPass<<<dimGrid,dimBlock>>>(devI1Ptr, dev01Ptr, devI2Ptr);
+		cudaThreadSynchronize();
+		cudaMemcpyFromSymbol(&bruteNumOut->x8, "bruteOut", sizeof(uint64_t),0,cudaMemcpyDeviceToHost);
+		bruteOut = 0;
+    }
+    
+        if(passPtr->x9 != 0)
+    {
+		cudaMemcpy(devI1Ptr, &passPtr->x9, sizeof(uint64_t), cudaMemcpyHostToDevice);
+		cudaMemcpy(dev01Ptr, &bruteNumOut->x9, sizeof(uint64_t), cudaMemcpyHostToDevice);
+		findPass<<<dimGrid,dimBlock>>>(devI1Ptr, dev01Ptr, devI2Ptr);
+		cudaThreadSynchronize();
+		cudaMemcpyFromSymbol(&bruteNumOut->x9, "bruteOut", sizeof(uint64_t),0,cudaMemcpyDeviceToHost);
+		bruteOut = 0;
+    }
+    
+        if(passPtr->x10 != 0)
+    {
+		cudaMemcpy(devI1Ptr, &passPtr->x10, sizeof(uint64_t), cudaMemcpyHostToDevice);
+		cudaMemcpy(dev01Ptr, &bruteNumOut->x10, sizeof(uint64_t), cudaMemcpyHostToDevice);
+		findPass<<<dimGrid,dimBlock>>>(devI1Ptr, dev01Ptr, devI2Ptr);
+		cudaThreadSynchronize();
+		cudaMemcpyFromSymbol(&bruteNumOut->x10, "bruteOut", sizeof(uint64_t),0,cudaMemcpyDeviceToHost);
+		bruteOut = 0;
+    }
+    
+        if(passPtr->x11 != 0)
+    {
+		cudaMemcpy(devI1Ptr, &passPtr->x11, sizeof(uint64_t), cudaMemcpyHostToDevice);
+		cudaMemcpy(dev01Ptr, &bruteNumOut->x11, sizeof(uint64_t), cudaMemcpyHostToDevice);
+		findPass<<<dimGrid,dimBlock>>>(devI1Ptr, dev01Ptr, devI2Ptr);
+		cudaThreadSynchronize();
+		cudaMemcpyFromSymbol(&bruteNumOut->x11, "bruteOut", sizeof(uint64_t),0,cudaMemcpyDeviceToHost);
+		bruteOut = 0;
+    }
+    
+        if(passPtr->x12 != 0)
+    {
+		cudaMemcpy(devI1Ptr, &passPtr->x12, sizeof(uint64_t), cudaMemcpyHostToDevice);
+		cudaMemcpy(dev01Ptr, &bruteNumOut->x12, sizeof(uint64_t), cudaMemcpyHostToDevice);
+		findPass<<<dimGrid,dimBlock>>>(devI1Ptr, dev01Ptr, devI2Ptr);
+		cudaThreadSynchronize();
+		cudaMemcpyFromSymbol(&bruteNumOut->x12, "bruteOut", sizeof(uint64_t),0,cudaMemcpyDeviceToHost);
+		bruteOut = 0;
+    }
+    
+            if(passPtr->x13 != 0)
+    {
+		cudaMemcpy(devI1Ptr, &passPtr->x13, sizeof(uint64_t), cudaMemcpyHostToDevice);
+		cudaMemcpy(dev01Ptr, &bruteNumOut->x13, sizeof(uint64_t), cudaMemcpyHostToDevice);
+		findPass<<<dimGrid,dimBlock>>>(devI1Ptr, dev01Ptr, devI2Ptr);
+		cudaThreadSynchronize();
+		cudaMemcpyFromSymbol(&bruteNumOut->x13, "bruteOut", sizeof(uint64_t),0,cudaMemcpyDeviceToHost);
+		bruteOut = 0;
+    }
+    
+            if(passPtr->x14 != 0)
+    {
+		cudaMemcpy(devI1Ptr, &passPtr->x14, sizeof(uint64_t), cudaMemcpyHostToDevice);
+		cudaMemcpy(dev01Ptr, &bruteNumOut->x14, sizeof(uint64_t), cudaMemcpyHostToDevice);
+		findPass<<<dimGrid,dimBlock>>>(devI1Ptr, dev01Ptr, devI2Ptr);
+		cudaThreadSynchronize();
+		cudaMemcpyFromSymbol(&bruteNumOut->x14, "bruteOut", sizeof(uint64_t),0,cudaMemcpyDeviceToHost);
+		bruteOut = 0;
+    }
     cudaError_t cudaErr;
     cudaErr = cudaGetLastError();
     printf("%s\n", cudaGetErrorString(cudaErr));
     cudaThreadSynchronize();
+    
     cudaFree(devI1Ptr);
     cudaFree(dev01Ptr);
     cudaFree(devI2Ptr);
     return;
 } 
 
-extern __global__ void findPass(passNum *passPtr, passNum *bruteNumOut, uint64_t *max)
+extern __global__ void findPass(uint64_t *passPtr, uint64_t *bruteNumOut, uint64_t *max)
 {
 	//each thread gets its own passNum to increment
 	uint64_t maxNum = *max;
-	passNum bruteNum = {0,0,0,0,0,0,0,0,333};
-	int bx = blockIdx.x;
-	int tx = threadIdx.x;
-	int tid = tx + 96*bx;
-	int stride = 96*43;
-	bruteNumOut->x9 = 456;
-	
-	//bruteNum starts at its tid, then increments by stride
-	bruteNum.x1 = tid;
-	bruteNum.x2 = tid;
-	bruteNum.x3 = tid;
-	bruteNum.x4 = tid;
-	bruteNum.x5 = tid;
-	bruteNum.x6 = tid;
-	bruteNum.x7 = tid;
-	bruteNum.x8 = tid;
-	bruteNum.x9 = tid;
-	
-	while(!isCracked)
-	{
-	/*
-		if(bruteNum.x8 >= maxNum){
-			bruteNum.x9 += stride;
-			bruteNum.x8 = maxNum;}
-		else if(bruteNum.x7 >= maxNum){
-			bruteNum.x8 += stride;
-			bruteNum.x7 = maxNum;}
-		else if(bruteNum.x6 >= maxNum){
-			bruteNum.x7 += stride;
-			bruteNum.x6 = maxNum;}
-		else if(bruteNum.x5 >= maxNum){
-			bruteNum.x6 += stride;
-			bruteNum.x5 = maxNum;}
-		else if(bruteNum.x4 >= maxNum){
-			bruteNum.x5 += stride;
-			bruteNum.x4 = maxNum;}
-		else if(bruteNum.x3 >= maxNum){
-			bruteNum.x4 += stride;
-			bruteNum.x3 = maxNum;}
-		else if(bruteNum.x2 >= maxNum){
-			bruteNum.x3 += stride;
-			bruteNum.x2 = maxNum;}
-		else if(bruteNum.x1 >= maxNum){
-			bruteNum.x2 += stride;
-			bruteNum.x1 = maxNum;}
-		else
-			bruteNum.x1 += stride;
-			*/
-			
-			bruteNum.x1 += stride;
-			if(bruteNum.x1 >= 500000)
-			{
-			*bruteNumOut = bruteNum;
-			bruteNumOut->x6 = maxNum;
-			isCracked = true;
-			}
-			//if we have found the password
-			if((bruteNum.x1 == passPtr->x1) &&
-			(bruteNum.x2 == passPtr->x2) &&
-			(bruteNum.x3 == passPtr->x3) &&
-			(bruteNum.x4 == passPtr->x4) &&
-			(bruteNum.x5 == passPtr->x5) &&
-			(bruteNum.x6 == passPtr->x6) &&
-			(bruteNum.x7 == passPtr->x7) &&
-			(bruteNum.x8 == passPtr->x8) &&
-			(bruteNum.x9 == passPtr->x9))
-			{
-			isCracked = true; //we're done
-			*bruteNumOut = bruteNum; //set output to the found password
-			}
-	}
-	
+	uint64_t bruteNum = 0;
+	uint64_t passNum = *passPtr;
+	int bx = blockIdx.x; //40960 blocks
+	int tx = threadIdx.x; //512 threads
+	int tid = tx + 512*bx;
+	int stride = 40960*512;
+	isCracked = false;
+
 	__syncthreads();
-	return;
+	if(tid <= passNum){
+		//bruteNum starts at its tid, then increments by stride
+		bruteNum = tid;
+
+		while(!isCracked)
+		{
+		
+				//if we have found the password
+				if(bruteNum >= passNum)
+				{
+					bruteOut = passNum;
+					//*bruteNumOut = bruteNum;
+					//set output to the found password
+				isCracked = true; //we're done
+				}
+
+				bruteNum += stride;
+		
+}}
+return;
 }
 
 
 main (int argc, char **argv) {
 	//variables
 	bool demoMode = 0;
-	passNum pass = {0,0,0,0,0,0,0,0,111};//Users entered password
+	passNum pass = {0,0,0,0,0,0,0,0,0};//Users entered password
 	passNum *passPtr = &pass;
-	passNum brute = {0,0,0,0,0,0,0,0,222};//programs cracked password to be found
+	passNum brute = {0,0,0,0,0,0,0,0,0};//programs cracked password to be found
 	passNum *brutePtr = &brute;
 	uint64_t *maxNumaz;
 	maxNumaz = (uint64_t*)malloc(sizeof(uint64_t));
-	*maxNumaz = 2580398988131886080;
+	*maxNumaz = 1250000000000; //line where the gpu may timeout
+	/*
+	az = 				8 zs	217180147158	
+	azAZ = 				6 zs	38736564342
+	azAZ09 =			6 zs	168705298125
+	azAZ09sc =			6 ~s	661055187436 
+	
+	*/
 	//take in user inputs   
 	char text[80];
 	if(demoMode)
@@ -165,7 +272,7 @@ main (int argc, char **argv) {
 	}
 	
 	char *str = &text[0];
-	stringToNumaz(str, passPtr);
+	stringToNumazAZ(str, passPtr);
 	printf("x1 = %llu \n", passPtr->x1);
 	printf("x2 = %llu \n", passPtr->x2);
 	printf("x3 = %llu \n", passPtr->x3);
@@ -198,16 +305,26 @@ void stringToNumaz(char *str, passNum *passPtr)
 			return;
 
 		
-		if(i > 64)
-			passPtr->x6 += (str[i] - 'a' + 1) * pow(26,i-65);
-		else if(i > 51)
-			passPtr->x5 += (str[i] - 'a' + 1) * pow(26,i-51);
-		else if(i > 38)
-			passPtr->x4 += (str[i] - 'a' + 1) * pow(26,i-39);
-		else if(i > 25)
-			passPtr->x3 += (str[i] - 'a' + 1) * pow(26,i-26);
-		else if(i > 12)
-			passPtr->x2 += (str[i] - 'a' + 1) * pow(26,i-13);
+		if(i > 79)
+			passPtr->x11 += (str[i] - 'a' + 1) * pow(26,i-80);
+		else if(i > 71)
+			passPtr->x10 += (str[i] - 'a' + 1) * pow(26,i-72);
+		else if(i > 63)
+			passPtr->x9 += (str[i] - 'a' + 1) * pow(26,i-64);
+		else if(i > 55)
+			passPtr->x8 += (str[i] - 'a' + 1) * pow(26,i-56);
+		else if(i > 47)
+			passPtr->x7 += (str[i] - 'a' + 1) * pow(26,i-48);
+		else if(i > 39)
+			passPtr->x6 += (str[i] - 'a' + 1) * pow(26,i-40);
+		else if(i > 31)
+			passPtr->x5 += (str[i] - 'a' + 1) * pow(26,i-32);
+		else if(i > 23)
+			passPtr->x4 += (str[i] - 'a' + 1) * pow(26,i-24);
+		else if(i > 15)
+			passPtr->x3 += (str[i] - 'a' + 1) * pow(26,i-16);
+		else if(i > 7)
+			passPtr->x2 += (str[i] - 'a' + 1) * pow(26,i-8);
 		else
 			passPtr->x1 += (str[i] - 'a' + 1) * pow(26,i);
 		}
@@ -224,22 +341,33 @@ void stringToNumazAZ(char *str, passNum *passPtr)
 		if(str[i] == '\n')
 			return;
 
-		if(i > 79)
-			passPtr->x9 += (str[i] - 'A' + 1) * pow(58,i-80);
-		else if(i > 69)
-			passPtr->x8 += (str[i] - 'A' + 1) * pow(58,i-70);
+
+		if(i > 77)
+			passPtr->x14 += (str[i] - 'A' + 1) * pow(58,i-78);
+		else if(i > 71)
+			passPtr->x13 += (str[i] - 'A' + 1) * pow(58,i-72);
+		else if(i > 65)
+			passPtr->x12 += (str[i] - 'A' + 1) * pow(58,i-66);
 		else if(i > 59)
-			passPtr->x7 += (str[i] - 'A' + 1) * pow(58,i-60);
-		else if(i > 49)
-			passPtr->x6 += (str[i] - 'A' + 1) * pow(58,i-50);
-		else if(i > 39)
-			passPtr->x5 += (str[i] - 'A' + 1) * pow(58,i-40);
+			passPtr->x11 += (str[i] - 'A' + 1) * pow(58,i-60);
+		else if(i > 53)
+			passPtr->x10 += (str[i] - 'A' + 1) * pow(58,i-54);
+		else if(i > 47)
+			passPtr->x9 += (str[i] - 'A' + 1) * pow(58,i-48);
+		else if(i > 41)
+			passPtr->x8 += (str[i] - 'A' + 1) * pow(58,i-42);
+		else if(i > 35)
+			passPtr->x7 += (str[i] - 'A' + 1) * pow(58,i-36);
 		else if(i > 29)
-			passPtr->x4 += (str[i] - 'A' + 1) * pow(58,i-30);
-		else if(i > 19)
-			passPtr->x3 += (str[i] - 'A' + 1) * pow(58,i-20);
-		else if(i > 9)
-			passPtr->x2 += (str[i] - 'A' + 1) * pow(58,i-10);
+			passPtr->x6 += (str[i] - 'A' + 1) * pow(58,i-30);
+		else if(i > 23)
+			passPtr->x5 += (str[i] - 'A' + 1) * pow(58,i-24);
+		else if(i > 17)
+			passPtr->x4 += (str[i] - 'A' + 1) * pow(58,i-18);
+		else if(i > 11)
+			passPtr->x3 += (str[i] - 'A' + 1) * pow(58,i-12);
+		else if(i > 5)
+			passPtr->x2 += (str[i] - 'A' + 1) * pow(58,i-6);
 		else
 			passPtr->x1 += (str[i] - 'A' + 1) * pow(58,i);
 		}
@@ -256,24 +384,34 @@ void stringToNumazAZ09(char *str, passNum *passPtr)
 		if(str[i] == '\n')
 			return;
 
-		if(i > 79)
-			passPtr->x9 += (str[i] - '0' + 1) * pow(74,i-80);
-		else if(i > 69)
-			passPtr->x8 += (str[i] - '0' + 1) * pow(74,i-70);
+		if(i > 77)
+			passPtr->x14 += (str[i] - '0' + 1) * pow(58,i-78);
+		else if(i > 71)
+			passPtr->x13 += (str[i] - '0' + 1) * pow(58,i-72);
+		else if(i > 65)
+			passPtr->x12 += (str[i] - '0' + 1) * pow(58,i-66);
 		else if(i > 59)
-			passPtr->x7 += (str[i] - '0' + 1) * pow(74,i-60);
-		else if(i > 49)
-			passPtr->x6 += (str[i] - '0' + 1) * pow(74,i-50);
-		else if(i > 39)
-			passPtr->x5 += (str[i] - '0' + 1) * pow(74,i-40);
+			passPtr->x11 += (str[i] - '0' + 1) * pow(58,i-60);
+		else if(i > 53)
+			passPtr->x10 += (str[i] - '0' + 1) * pow(58,i-54);
+		else if(i > 47)
+			passPtr->x9 += (str[i] - '0' + 1) * pow(58,i-48);
+		else if(i > 41)
+			passPtr->x8 += (str[i] - '0' + 1) * pow(58,i-42);
+		else if(i > 35)
+			passPtr->x7 += (str[i] - '0' + 1) * pow(58,i-36);
 		else if(i > 29)
-			passPtr->x4 += (str[i] - '0' + 1) * pow(74,i-30);
-		else if(i > 19)
-			passPtr->x3 += (str[i] - '0' + 1) * pow(74,i-20);
-		else if(i > 9)
-			passPtr->x2 += (str[i] - '0' + 1) * pow(74,i-10);
-		else 
-			passPtr->x1 += (str[i] - '0' + 1) * pow(74,i);
+			passPtr->x6 += (str[i] - '0' + 1) * pow(58,i-30);
+		else if(i > 23)
+			passPtr->x5 += (str[i] - '0' + 1) * pow(58,i-24);
+		else if(i > 17)
+			passPtr->x4 += (str[i] - '0' + 1) * pow(58,i-18);
+		else if(i > 11)
+			passPtr->x3 += (str[i] - '0' + 1) * pow(58,i-12);
+		else if(i > 5)
+			passPtr->x2 += (str[i] - '0' + 1) * pow(58,i-6);
+		else
+			passPtr->x1 += (str[i] - '0' + 1) * pow(58,i);
 		}
 		
 	return;
@@ -288,24 +426,34 @@ void stringToNumazAZ09sc(char *str, passNum *passPtr)
 		if(str[i] == '\n')
 			return;
 
-		if(i > 71)
-			passPtr->x9 += (str[i] - '!' + 1) * pow(93,i-72);
-		else if(i > 62)
-			passPtr->x8 += (str[i] - '!' + 1) * pow(93,i-63);
+		if(i > 77)
+			passPtr->x14 += (str[i] - '!' + 1) * pow(58,i-78);
+		else if(i > 71)
+			passPtr->x13 += (str[i] - '!' + 1) * pow(58,i-72);
+		else if(i > 65)
+			passPtr->x12 += (str[i] - '!' + 1) * pow(58,i-66);
+		else if(i > 59)
+			passPtr->x11 += (str[i] - '!' + 1) * pow(58,i-60);
 		else if(i > 53)
-			passPtr->x7 += (str[i] - '!' + 1) * pow(93,i-54);
-		else if(i > 44)
-			passPtr->x6 += (str[i] - '!' + 1) * pow(93,i-45);
+			passPtr->x10 += (str[i] - '!' + 1) * pow(58,i-54);
+		else if(i > 47)
+			passPtr->x9 += (str[i] - '!' + 1) * pow(58,i-48);
+		else if(i > 41)
+			passPtr->x8 += (str[i] - '!' + 1) * pow(58,i-42);
 		else if(i > 35)
-			passPtr->x5 += (str[i] - '!' + 1) * pow(93,i-36);
-		else if(i > 26)
-			passPtr->x4 += (str[i] - '!' + 1) * pow(93,i-27);
+			passPtr->x7 += (str[i] - '!' + 1) * pow(58,i-36);
+		else if(i > 29)
+			passPtr->x6 += (str[i] - '!' + 1) * pow(58,i-30);
+		else if(i > 23)
+			passPtr->x5 += (str[i] - '!' + 1) * pow(58,i-24);
 		else if(i > 17)
-			passPtr->x3 += (str[i] - '!' + 1) * pow(93,i-18);
-		else if(i > 8)
-			passPtr->x2 += (str[i] - '!' + 1) * pow(93,i-9);
+			passPtr->x4 += (str[i] - '!' + 1) * pow(58,i-18);
+		else if(i > 11)
+			passPtr->x3 += (str[i] - '!' + 1) * pow(58,i-12);
+		else if(i > 5)
+			passPtr->x2 += (str[i] - '!' + 1) * pow(58,i-6);
 		else
-			passPtr->x1 += (str[i] - '!' + 1) * pow(93,i);
+			passPtr->x1 += (str[i] - '!' + 1) * pow(58,i);
 		}
 		
 	return;
